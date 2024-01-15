b785ad529d108e4df6520cf58c627e9d30cd1720
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2024-01-14 21:32:01 -0800

    initial commit

---
+++ src/main/java/com/s82033788/CPEN431/A4/App.java
@@ -0,0 +1,13 @@
+package com.s82033788.CPEN431.A4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
