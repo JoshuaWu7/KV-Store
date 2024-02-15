2838de99c6eb5448ad3ed8226bd5a3d50628b1d8
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2024-02-14 18:05:54 -0800

    added concurrency support to ConsistentMap

--- src/main/java/com/s82033788/CPEN431/A4/wrappers/ConsistentMap.java
+++ src/main/java/com/s82033788/CPEN431/A4/wrappers/ConsistentMap.java
@@ -7,14 +7,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ConsistentMap {
     private final TreeMap<Integer, InetAddress> ring;
     private final int vnodes;
+    private final ReadWriteLock lock;
 
     public ConsistentMap(int vnodes, String serverPathName) throws IOException {
         this.ring = new TreeMap<>();
         this.vnodes = vnodes;
+        this.lock = new ReentrantReadWriteLock();
 
         Path path = Paths.get(serverPathName);
         List<String> serverList = Files.readAllLines(path , StandardCharsets.UTF_8);
@@ -28,32 +32,42 @@ public class ConsistentMap {
 
     public void addServer(InetAddress address)
     {
+        lock.writeLock().lock();
         for(int i = 0; i < vnodes; i++)
         {
             int hashcode = Objects.hash(address, i);
             ring.put(hashcode, address);
         }
+        lock.writeLock().unlock();
     }
 
     public void removeServer(InetAddress address)
     {
+        lock.writeLock().lock();
         for(int i = 0; i < vnodes; i++)
         {
             int hashcode = Objects.hash(address, i);
             ring.remove(hashcode);
         }
+        lock.writeLock().unlock();
     }
 
     public InetAddress getServer(byte[] key) throws NoServersException {
-        if(ring.isEmpty()) throw new NoServersException();
+        lock.readLock().lock();
+        if(ring.isEmpty())
+        {
+            lock.readLock().unlock();
+            throw new NoServersException();
+        }
 
         int hashcode = Arrays.hashCode(key);
 
         Map.Entry<Integer, InetAddress> server = ring.floorEntry(hashcode);
+        /* Deal with case where the successor of the key is past "0" */
+        server = (server == null) ? ring.firstEntry(): server;
 
-        //case where we have to wrap around the ring.
-        if(server == null) return ring.firstEntry().getValue();
-        //normal case
+        lock.readLock().unlock();
+
         return server.getValue();
     }
 
