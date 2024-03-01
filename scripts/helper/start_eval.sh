5d151220c8dfb1651806f426fc9df7741e5ca379
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2024-02-29 23:12:19 -0800

    added A7.jar, removed eval clients

--- scripts/helper/start_eval.sh
+++ scripts/helper/start_eval.sh
@@ -4,7 +4,6 @@ echo "starting test"
 cd testjar
 java -jar a7_eval.jar --servers-list=servers_all.txt --submit --secret-code 2706426568
 
-#java -jar a7_eval.jar --servers-list servers_all.txt
 
 
 echo "killing all java"
