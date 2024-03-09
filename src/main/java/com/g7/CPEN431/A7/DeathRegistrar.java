5ce7c1e6d7ad4c286ccf3172a3a0cd5b1f426abb
chen-vv <vchen03@student.ubc.ca>
2024-03-09 12:23:12 -0800

    Renamed deathRecord related variables

--- src/main/java/com/g7/CPEN431/A7/DeathRegistrar.java
+++ src/main/java/com/g7/CPEN431/A7/DeathRegistrar.java
@@ -16,7 +16,7 @@ import static com.g7.CPEN431.A7.KVServer.*;
 import static com.g7.CPEN431.A7.consistentMap.ServerRecord.CODE_DED;
 
 public class DeathRegistrar extends TimerTask {
-    Map<ServerRecord, ServerRecord> deathRecord;
+    Map<ServerRecord, ServerRecord> broadcastQueue;
     ConcurrentLinkedQueue<ServerRecord> pendingRecords;
     ConsistentMap ring;
     KVClient sender;
@@ -28,7 +28,7 @@ public class DeathRegistrar extends TimerTask {
 
     public DeathRegistrar(ConcurrentLinkedQueue<ServerRecord> pendingRecords, ConsistentMap ring)
     throws IOException {
-        this.deathRecord = new HashMap<>();
+        this.broadcastQueue = new HashMap<>();
         this.pendingRecords = pendingRecords;
         this.ring = ring;
         this.sender = new KVClient(null, 0, new DatagramSocket(), new byte[16384]);
@@ -38,18 +38,18 @@ public class DeathRegistrar extends TimerTask {
 
     @Override
     public void run() {
-        updateDeathRecords();
+        updateBroadcastQueue();
         checkSelfSuspended();
         checkIsAlive();
         gossip();
     }
 
-    private void updateDeathRecords()
+    private void updateBroadcastQueue()
     {
         ServerRecord n;
         while((n = pendingRecords.poll()) != null)
         {
-            ServerRecord existing = deathRecord.get(n);
+            ServerRecord existing = broadcastQueue.get(n);
 
             if((existing != null && !existing.hasInformationTime()) || !n.hasInformationTime())
             {
@@ -61,7 +61,7 @@ public class DeathRegistrar extends TimerTask {
                 continue;
             }
 
-            deathRecord.put(n,n);
+            broadcastQueue.put(n,n);
         }
     }
 
@@ -80,13 +80,13 @@ public class DeathRegistrar extends TimerTask {
             throw new IllegalStateException();
         }
 
-        if(deathRecord.isEmpty())
+        if(broadcastQueue.isEmpty())
         {
             return;
         }
 
         sender.setDestination(target.getAddress(), target.getServerPort());
-        List<ServerEntry> l = new ArrayList<>(deathRecord.values());
+        List<ServerEntry> l = new ArrayList<>(broadcastQueue.values());
         ServerResponse r;
         try {
             r = sender.isDead(l);
@@ -95,7 +95,7 @@ public class DeathRegistrar extends TimerTask {
             System.out.println("gossipee is dead");
             ring.setServerDeadNow(target);
             ring.removeServer(target);
-            deathRecord.put(target, target);
+            broadcastQueue.put(target, target);
             return;
         } catch (Exception e) {
             System.out.println("Spreading gossip failed");
@@ -129,7 +129,7 @@ public class DeathRegistrar extends TimerTask {
             if(responses.get(i) == KVServerTaskHandler.STAT_CODE_OLD && random.nextInt(K) == 0)
             {
                 //delete the response
-                deathRecord.remove( (ServerRecord) l.get(i));
+                broadcastQueue.remove( (ServerRecord) l.get(i));
                 //TODO additional logic to store old "news" that is recirculating. probably let vanessa do this.
             }
         }
@@ -166,7 +166,7 @@ public class DeathRegistrar extends TimerTask {
             // Update server death time to current time, then add to list of deaths
             ring.setServerDeadNow(target);
             ring.removeServer(target);
-            deathRecord.put(target, target);
+            broadcastQueue.put(target, target);
         }
     }
 
@@ -186,7 +186,7 @@ public class DeathRegistrar extends TimerTask {
                 self.setLastSeenNow();
                 ring.setServerAlive(self);
 
-                deathRecord.put(self, self);
+                broadcastQueue.put(self, self);
             }
         }
     }
